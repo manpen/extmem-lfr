f946a255e0c10b3c22e9b982ccb6b11988d22020
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2015-07-25 17:31:45 +0200

    Another implemention of HH intended to reduce the number of IO caused by the prio queue; benchmarks will follow; correctness not checked yet

---
+++ HavelHakimiGeneratorRLE.h
@@ -0,0 +1,220 @@
+#pragma once
+
+#include <stxxl/types>
+#include <stxxl/priority_queue>
+#include <stxxl/stack>
+#include <utility>
+
+template <typename InputStream>
+class HavelHakimiGeneratorRLE {
+public:
+    typedef std::pair<stxxl::uint64, stxxl::uint64> value_type;
+
+private:
+    using node_block_type = typename InputStream::value_type;
+    struct ComparatorLess
+    {
+        bool operator () (const node_block_type& a, const node_block_type & b) const { 
+            // basic lexicographical ordering
+            return (a.value < b.value) || (a.value == b.value && a.index < b.index); 
+        }
+        
+        node_block_type min_value() const { 
+            return {
+                .value = std::numeric_limits<typename node_block_type::value_type>::min(),
+                .count = 0,
+                .index = 0
+            };
+        }
+    };
+
+    using pqueue_type = typename stxxl::PRIORITY_QUEUE_GENERATOR<node_block_type, ComparatorLess, 16*1024*1024, 1024*1024>::result;
+    using pq_block_type = typename pqueue_type::block_type;
+    stxxl::read_write_pool<pq_block_type> _pool;
+    pqueue_type _prioQueue;
+
+    using node_degree_stack_type = typename stxxl::STACK_GENERATOR<node_block_type, stxxl::external, stxxl::grow_shrink>::result;
+    node_degree_stack_type _buffer;
+
+    stxxl::int64 _current_node;
+    stxxl::int64 _current_node_degree;
+    stxxl::int64 _current_partner_block_nodes_left;
+    stxxl::int64 _current_partner_node;
+    
+    value_type _current_edge;
+
+    stxxl::uint64 _num_edges;
+
+    bool _empty;
+    
+
+    // Moves elements from the buffer back into the priority queue
+    // If two neighbouring blocks of the same degree with sequential indicies are found,
+    // they are automatically merge in order to compensate for fragmentation
+    void _write_buffer_back() {
+#if 1
+        // variant that allows for fragmentation
+        while(!_buffer.empty()) {
+            _prioQueue.push(_buffer.top());
+            _buffer.pop();
+        }
+        
+#else        
+        if (_buffer.empty())
+            return;
+        
+        node_block_type this_block;
+        if (_prioQueue.empty()) {
+            this_block = _buffer.top();
+            _buffer.pop();
+        } else {
+            this_block = _prioQueue.top();
+            _prioQueue.pop();
+        }
+            
+        while(!_buffer.empty()) {
+            node_block_type block = _buffer.top();
+            _buffer.pop();
+            
+            // TODO: Change order of block/this_block if buffer is a FIFO
+            if ( (this_block.value == block.value) && (block.index - block.count == this_block.index) ) {
+                // we can merge those blocks !
+                block.count += this_block.count;
+                
+            } else {
+                _prioQueue.push(this_block);
+                
+            }
+            
+            this_block = block;
+        }
+        
+        _prioQueue.push(this_block);
+#endif        
+    }
+    
+    // helper function that will reduce the degree of up to #nodes_to_consume of the block
+    // provided by one and then buffer this block (or the resulting two blocks of different degrees)
+    void _consume_and_buffer_block(node_block_type & block, stxxl::uint64 nodes_to_consume) {
+        assert(block.index >= block.count);
+        
+        if (!block.count)
+            return;
+        
+        if (nodes_to_consume >= block.count) {
+            // whole block will be consumed, i.e. reduce degree of each node
+            if (--block.value)
+                _buffer.push(block);
+
+            // set partner information
+            _current_partner_node = block.index;
+            _current_partner_block_nodes_left = block.count;
+            
+        } else {
+            // split block and consume second half
+            node_block_type new_block = {
+                .value = block.value - 1,
+                .count = nodes_to_consume,
+                .index = block.index - block.count + nodes_to_consume
+            };
+            block.count -= nodes_to_consume;
+
+            _buffer.push(block);
+            
+            if (new_block.value)
+                _buffer.push(new_block);
+
+            // set partner information
+            _current_partner_node = new_block.index;
+            _current_partner_block_nodes_left = new_block.count;            
+        }
+    }
+    
+    void _generate_next_edge() {
+        if ( UNLIKELY(!_current_node_degree) ) {
+            // we're done with the current node: write buffer back and (try to) fetch next node
+            _write_buffer_back();
+        
+            if ( UNLIKELY(_prioQueue.empty()) ) {
+                // all blocks consumed: nothing left to do 
+                _empty = true;
+                return;
+            }
+        }
+        
+        if (!_current_node_degree) {
+            auto block = _prioQueue.top();
+            _prioQueue.pop();
+
+            // we remove the first node from the block, so the block's index and size have to be adapted
+            _current_node = block.index--;
+            block.count--;
+            
+            _current_node_degree = block.value;
+            _consume_and_buffer_block(block, _current_node_degree);
+            
+            //std::cout << "Started with node " << _current_node << " and requested degree of " << _current_node_degree << std::endl;
+            
+            assert(_current_node_degree);
+        }
+        
+        if (!_current_partner_block_nodes_left) {
+            if ( UNLIKELY(_prioQueue.empty()) ) {
+                STXXL_ERRMSG(
+                    "Degree sequence not realizable, node " << _current_node <<
+                    " should have gotten " << _current_node_degree << " more neighbors"
+                );
+                
+                // invalid degree sequence; let's reduce this nodes degree and move to the next node
+                _current_node_degree = 0;
+                _generate_next_edge();
+                return;
+                
+            } else {
+                auto block = _prioQueue.top();
+                _prioQueue.pop();
+                _consume_and_buffer_block(block, _current_node_degree);
+            }
+        }
+        
+        
+        _current_edge = {_current_node, _current_partner_node--};
+        _current_node_degree--;
+        _current_partner_block_nodes_left--;
+    }
+    
+    
+public:
+    HavelHakimiGeneratorRLE(InputStream &input) 
+        : _pool(static_cast<size_t>(8*1024*1024/pq_block_type::raw_size), static_cast<size_t>(8*1024*1024/pq_block_type::raw_size))
+        , _prioQueue(_pool)
+        , _empty(false) 
+    {
+        _num_edges = 0;
+
+        for (; !input.empty(); ++input) {
+            auto block = *input;
+            _prioQueue.push(block);
+            _num_edges += block.value * block.count;
+        }
+
+        _num_edges /= 2;
+        
+        // state that will fetch the "next" (i.e. the first) node properly from the prio queue
+        _current_node_degree = 0;
+        _current_partner_block_nodes_left = 0;
+        _generate_next_edge();
+    }
+
+    const value_type & operator * () const { return _current_edge; };
+    const value_type * operator -> () const { return &_current_edge; };
+    
+    HavelHakimiGeneratorRLE & operator++ () {
+        _generate_next_edge();
+        return *this;
+    }
+    
+    bool empty() const { return _empty; };
+
+    stxxl::uint64 maxEdges() const { return _num_edges; };
+};
--- test1.cpp
+++ test1.cpp
@@ -1,41 +1,68 @@
 #include <iostream>
+#include <chrono>
 
 #include <stxxl/cmdline>
-
 #include <stxxl/vector>
+
 #include "PowerlawDegreeSequence.h"
 #include "HavelHakimiGenerator.h"
 
-struct RunConfig {
-   stxxl::uint64 numNodes;
-   stxxl::uint64 minDeg;
-   stxxl::uint64 maxDeg;
-   double gamma;
-   
-   RunConfig() 
-      : numNodes(10 * 1024 * 1024)
-      , minDeg(2)
-      , maxDeg(100000)
-      , gamma(-2.0)
-   {} 
-   
-   bool parse_cmdline(int argc, char* argv[]) {
-      stxxl::cmdline_parser cp;
-      cp.add_opt_param_bytes("num-nodes", numNodes, "Generate # nodes, Default: 10 Mi");
-      cp.add_opt_param_bytes("min-deg", minDeg, "Min. Deg of Powerlaw Deg. Distr.");
-      cp.add_opt_param_bytes("max-deg", maxDeg, "Max. Deg of Powerlaw Deg. Distr.");
-      cp.add_opt_param_double("gamma", gamma, "Gamma of Powerlaw Deg. Distr.");
-      
+#include "DistributionCount.h"
+#include "HavelHakimiGeneratorRLE.h"
 
-      if (!cp.process(argc, argv)) {
-         cp.print_usage();
-         return false;
-      }
-      
-      cp.print_result();
-      return true;
-   }
+#include "DegreeDistributionCheck.h"
+
+
+struct RunConfig {
+    stxxl::uint64 numNodes;
+    stxxl::uint64 minDeg;
+    stxxl::uint64 maxDeg;
+    double gamma;
+
+    bool rle;
+    bool showInitDegrees;
+    bool showResDegrees;
+
+    unsigned int randomSeed;
+
+    RunConfig() 
+        : numNodes(10 * 1024 * 1024)
+        , minDeg(2)
+        , maxDeg(100000)
+        , gamma(-2.0)
+        , rle(false)
+        , showInitDegrees(false)
+        , showResDegrees(false)
+    {
+        using myclock = std::chrono::high_resolution_clock;
+        myclock::duration d = myclock::now() - myclock::time_point::min();
+        randomSeed = d.count();
+    }
+
+
+    bool parse_cmdline(int argc, char* argv[]) {
+        stxxl::cmdline_parser cp;
+
+        cp.add_bytes ('n', "num-nodes", numNodes, "Generate # nodes, Default: 10 Mi");
+        cp.add_bytes ('a', "min-deg",   minDeg,   "Min. Deg of Powerlaw Deg. Distr.");
+        cp.add_bytes ('b', "max-deg",   maxDeg,   "Max. Deg of Powerlaw Deg. Distr.");
+        cp.add_double('g', "gamma",     gamma,    "Gamma of Powerlaw Deg. Distr.");
+        cp.add_uint  ('s', "seed",      randomSeed,   "Initial seed for PRNG");
+        
+        cp.add_flag  ('r', "rle",       rle,          "Use RLE HavelHakimi");
+        cp.add_flag  ('i', "init-degrees", showInitDegrees, "Output requested degrees (no HH gen)");
+        cp.add_flag  ('d', "res-degrees",  showResDegrees,  "Output degree distribution of result");
+
+        if (!cp.process(argc, argv)) {
+            cp.print_usage();
+            return false;
+        }
+
+        cp.print_result();
+        return true;
+    }
 };
+
 
 void benchmark(RunConfig & config) {
     stxxl::stats * stats = stxxl::stats::get_instance();
@@ -43,27 +70,61 @@ void benchmark(RunConfig & config) {
     
     PowerlawDegreeSequence degreeSequence(config.minDeg, config.maxDeg, config.gamma, config.numNodes);
 
-    HavelHakimiGenerator hhgenerator(degreeSequence);
-
-    std::cout << "Stats after filling of prio queue:" << (stxxl::stats_data(*stats) - stats_begin);
+    if (config.showInitDegrees) {
+        DistributionCount<PowerlawDegreeSequence> dcount(degreeSequence);
+        
+        for(; !dcount.empty(); ++dcount) {
+            auto block = *dcount;
+            std::cout << block.value << " " << block.count << " # InitDD  Degree Count" << std::endl;
+        }
+        
+        return;
+    }
     
     // create vector
     typedef stxxl::VECTOR_GENERATOR<HavelHakimiGenerator::value_type>::result result_vector_type;
-    result_vector_type vector(hhgenerator.maxEdges());
+    result_vector_type edges;
+    
+    if (config.rle) {
+        DistributionCount<PowerlawDegreeSequence> dcount(degreeSequence);
+        HavelHakimiGeneratorRLE<DistributionCount<PowerlawDegreeSequence>> hhgenerator(dcount);
+        std::cout << "Stats after filling of prio queue:" << (stxxl::stats_data(*stats) - stats_begin);
+        
+        edges.resize(hhgenerator.maxEdges());
+        auto endIt = stxxl::stream::materialize(hhgenerator, edges.begin());
+        edges.resize(endIt - edges.begin());
 
-    auto endIt = stxxl::stream::materialize(hhgenerator, vector.begin());
-    vector.resize(endIt - vector.begin());
-
-    std::cout << "Generated " << vector.size() << " edges of possibly " << hhgenerator.maxEdges() << " edges" << std::endl;
-
+        std::cout << "Generated " << edges.size() << " edges of possibly " << hhgenerator.maxEdges() << " edges" << std::endl;
+        
+    } else {
+        HavelHakimiGenerator hhgenerator(degreeSequence);
+        std::cout << "Stats after filling of prio queue:" << (stxxl::stats_data(*stats) - stats_begin);
+        
+        edges.resize(hhgenerator.maxEdges());
+        auto endIt = stxxl::stream::materialize(hhgenerator, edges.begin());
+        edges.resize(endIt - edges.begin());
+        std::cout << "Generated " << edges.size() << " edges of possibly " << hhgenerator.maxEdges() << " edges" << std::endl;
+    }
+    
     std::cout << (stxxl::stats_data(*stats) - stats_begin);
     
-    
 #if 0
-    for (auto edge : vector) {
-        std::cout << edge.first << ", " << edge.second << std::endl;
+    unsigned int i = 0;
+    for (auto edge : edges) {
+        std::cout << i++ << ": " << edge.first << ", " << edge.second << std::endl;
     }
 #endif
+
+    if (config.showResDegrees) {
+        DegreeDistributionCheck<result_vector_type::const_iterator> ddc {edges.begin(), edges.end()};
+        
+        auto deg_distr = ddc.getDistribution();
+
+        for(; !deg_distr.empty(); ++deg_distr) {
+            auto block = *deg_distr;
+            std::cout << block.value << " " << block.count << " # ResDD  Degree Count" << std::endl;
+        }
+    }
 }
 
 
@@ -72,6 +133,8 @@ int main(int argc, char* argv[]) {
    RunConfig config;
    if (!config.parse_cmdline(argc, argv))
       return -1;
+   
+   stxxl::srandom_number32(config.randomSeed);
    
    benchmark(config);
    return 0;
