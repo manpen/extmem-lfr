f540f396bea1a4660d2c1c21ac91ae450c67894a
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2015-12-31 18:19:40 +0100

    Moved statistics helpers to EdgeSwapBase and fixed indention

--- include/EdgeSwapBase.h
+++ include/EdgeSwapBase.h
@@ -4,44 +4,77 @@
 
 class EdgeSwapBase {
 protected:
-   bool _display_debug;
+    bool _display_debug;
 
-   std::pair<edge_t, edge_t> _swap_edges(const edge_t & e0, const edge_t & e1, bool direction) const {
-      edge_t t0, t1;
-      if (direction) {
-         if (e0.second < e1.first) {
-            t0 = {e0.second, e1.first};
-         } else {
-            t0 = {e1.first, e0.second};
-         }
-         if (e0.first < e1.second) {
-            t1 = {e0.first, e1.second};
-         } else {
-            t1 = {e1.second, e0.first};
-         }
-      } else {
-         if (e1.first < e0.first) {
-            t0 = {e1.first, e0.first};
-         } else {
-            t0 = {e0.first, e1.first};
-         }
-         if (e0.second < e1.second) {
-            t1 = {e0.second, e1.second};
-         } else {
-            t1 = {e1.second, e0.second};
-         }
-      }
+    std::pair<edge_t, edge_t> _swap_edges(const edge_t & e0, const edge_t & e1, bool direction) const {
+        edge_t t0, t1;
+        if (direction) {
+            if (e0.second < e1.first) {
+                t0 = {e0.second, e1.first};
+            } else {
+                t0 = {e1.first, e0.second};
+            }
+            if (e0.first < e1.second) {
+                t1 = {e0.first, e1.second};
+            } else {
+                t1 = {e1.second, e0.first};
+            }
+        } else {
+            if (e1.first < e0.first) {
+                t0 = {e1.first, e0.first};
+            } else {
+                t0 = {e0.first, e1.first};
+            }
+            if (e0.second < e1.second) {
+                t1 = {e0.second, e1.second};
+            } else {
+                t1 = {e1.second, e0.second};
+            }
+        }
 
-      return std::make_pair(t0, t1);
-   }
+        return std::make_pair(t0, t1);
+    }
+
+    stxxl::stats_data _stats;
+
+    //! Start a statistics counter that will be reported using _report_stats
+    void _start_stats(bool compute_stats = true) {
+        if (!compute_stats) return;
+        _stats = stxxl::stats_data(*stxxl::stats::get_instance());
+    }
+
+    //! Report I/O and time statistics and restarts counters
+    //! @warning Function reports only relative values, so call _start_stats before using
+    void _report_stats(const std::string &prefix, bool compute_stats = true) {
+        if (!compute_stats) return;
+
+        auto start = _stats;
+        _start_stats();
+        std::ostringstream ss;
+        ss << (_stats - start);
+
+        std::string str = ss.str();
+        std::string replace = "\n" + prefix;
+        str = prefix + str;
+
+        size_t pos = prefix.size();
+        while (1) {
+            pos = str.find("\n", pos);
+            if (pos == std::string::npos) break;
+            str.replace(pos, 1, replace);
+            pos += replace.length();
+        }
+
+        std::cout << str << std::endl;
+    }
 
 public:
-   EdgeSwapBase() :
-         _display_debug(false)
-   {}
+    EdgeSwapBase() :
+          _display_debug(false)
+    {}
 
-   void setDisplayDebug(bool v) {
-      _display_debug = v;
-   }
+    void setDisplayDebug(bool v) {
+        _display_debug = v;
+    }
 
 };
