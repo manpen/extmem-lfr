c70d3c7a41e1761b6c2d866dab0b5c0e0f746632
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2016-01-20 00:25:49 +0100

    Add Simple EM Queue for boolean which either supports writing (push) or reading (stream)

---
+++ include/BoolStream.hpp
@@ -0,0 +1,122 @@
+#pragma once
+/**
+ * @file
+ * @brief  Bufreader to edge vector with merger of updated edges
+ * @author Manuel Penschuck
+ * @copyright to be decided
+ */
+
+#include <stxxl/queue>
+
+class BoolStream : stxxl::noncopyable {
+    using T = std::uint64_t;
+
+    enum Mode {WRITING, READING};
+    Mode _mode;
+
+    // the big picture
+    stxxl::queue<T> _queue;
+    uint64_t _items_stored;
+
+    // word-wise buffer
+    T _buffered_word;
+    uint _remaining_bits;
+    constexpr static uint _max_bits_buffer = sizeof(T) * 8;
+    constexpr static T _msb = T(1) << (_max_bits_buffer - 1);
+
+    void _fetch_word() {
+        assert(!_queue.empty());
+
+        _buffered_word = _queue.front();
+        _queue.pop();
+
+        _remaining_bits = _max_bits_buffer;
+    }
+
+public:
+    // Let's satisfy the big 5 ;)
+    BoolStream() {clear();}
+    ~BoolStream() = default;
+    BoolStream(const BoolStream&) = delete;
+    BoolStream(BoolStream&& other) {
+        swap(other);
+    }
+    BoolStream& operator=(BoolStream&& other) {
+        swap(other);
+        return *this;
+    }
+
+    void swap(BoolStream& other) {
+        std::swap(_mode, other._mode);
+        _queue.swap(other._queue);
+        std::swap(_items_stored, other._items_stored);
+        std::swap(_buffered_word, other._buffered_word);
+        std::swap(_remaining_bits, other._remaining_bits);
+    }
+
+
+    //! Clear structure and switch into write mode
+    void clear() {
+        while(!_queue.empty())
+            _queue.pop();
+
+        _mode = WRITING;
+        _remaining_bits = _max_bits_buffer;
+        _items_stored = 0;
+    }
+
+    //! Add a new data item
+    void push(bool v) {
+        assert(_mode == WRITING);
+
+        _buffered_word = (_buffered_word << 1) | v;
+        _remaining_bits--;
+        _items_stored++;
+
+        if (!_remaining_bits) {
+            _queue.push(_buffered_word);
+            _remaining_bits = _max_bits_buffer;
+        }
+    }
+
+    //! Switch to reading mode, make the stream interface available
+    void consume() {
+        if (_remaining_bits != _max_bits_buffer) {
+            _queue.push(_buffered_word << _remaining_bits);
+        }
+
+        _mode = READING;
+
+        if (_items_stored)
+            _fetch_word();
+    }
+
+//! @name STXXL Streaming Interface
+//! @{
+    BoolStream& operator++() {
+        assert(_mode == READING);
+        assert(!empty());
+
+        _buffered_word <<= 1;
+        --_remaining_bits;
+        --_items_stored;
+
+        if (UNLIKELY(!_remaining_bits && _items_stored))
+            _fetch_word();
+
+        return *this;
+    }
+
+    bool operator*() const {
+        assert(_mode == READING);
+        assert(!empty());
+
+        return _buffered_word & _msb;
+    }
+
+    bool empty() const {
+        assert(_mode == READING);
+        return !_items_stored;
+    }
+//! @}
+};
---
+++ test/TestBoolStream.cpp
@@ -0,0 +1,105 @@
+/**
+ * @file
+ * @brief Test cases for TupleHelpers
+ * @author Manuel Penschuck
+ * @copyright
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @copyright
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+
+#include <random>
+#include <BoolStream.hpp>
+
+class TestBoolStream : public ::testing::Test {};
+
+TEST_F(TestBoolStream, smallFillAndConsume) {
+    for(uint i=0; i < 2; i++) {
+        bool v = i;
+        BoolStream bs;
+        bs.push(v);
+        bs.push(!v);
+
+        bs.consume();
+        ASSERT_FALSE(bs.empty());
+        ASSERT_EQ(*bs, v);
+        ++bs;
+        ASSERT_FALSE(bs.empty());
+        ASSERT_EQ(*bs, !v);
+        ++bs;
+        ASSERT_TRUE(bs.empty());
+    }
+}
+
+TEST_F(TestBoolStream, basicFillAndConsume) {
+    constexpr uint max_fills = 1000;
+
+    for(uint i=0; i < 2; i++) {
+        bool v = i;
+        BoolStream bs;
+
+        for(uint fill_size = 0; fill_size < max_fills; ++fill_size) {
+            bs.push(v);
+            for(uint j=fill_size; j; --j)
+                bs.push(!v);
+        }
+
+        bs.consume();
+
+        uint64_t idx = 0;
+
+        for(uint fill_size = 0; fill_size < max_fills; ++fill_size) {
+            ASSERT_FALSE(bs.empty()) << idx;
+            ASSERT_EQ(*bs, v) << idx;
+            ++bs; ++idx;
+            for(uint j=fill_size; j; --j) {
+                ASSERT_FALSE(bs.empty()) << idx;
+                ASSERT_EQ(*bs, !v) << idx;
+                ++bs; ++idx;
+            }
+        }
+
+        ASSERT_TRUE(bs.empty());
+    }
+}
+
+TEST_F(TestBoolStream, randomFillAndConsume) {
+    BoolStream bs;
+    stxxl::random_number32 rand;
+    for(unsigned int round = 0; round < 100; round++) {
+        unsigned int write_items = rand(1 << 20);
+        unsigned int read_items = rand(write_items);
+
+        std::vector<bool> reference;
+        reference.reserve(write_items);
+
+        while(write_items) {
+            uint32_t w = rand(~uint32_t(0));
+            for (unsigned int word = 32; write_items && word; --write_items, --word) {
+                bool v = w & 1;
+                w >>= 1;
+                reference.push_back(v);
+                bs.push(v);
+            }
+        }
+
+        bs.consume();
+
+        for(unsigned int i=0; i<read_items; i++) {
+            ASSERT_FALSE(bs.empty()) << i;
+            ASSERT_EQ(*bs, reference[i]) << i;
+            ++bs;
+        }
+
+        bs.clear();
+    }
+}
