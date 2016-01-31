c57864267e702cd6782b469f94a0e8ffcb8162f1
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2016-01-31 11:47:06 +0100

    Adopt PowerlawDegreeSequence to types in defs.h and added additional config struct

--- include/PowerlawDegreeSequence.cpp
+++ include/PowerlawDegreeSequence.cpp
@@ -1,12 +1,14 @@
-//
-// Created by michael on 08.07.15.
-//
-
+/**
+ * @file
+ * @brief Parameterizable Power-Law Distribution as STXXL stream
+ * @author Michael Hamann
+ * @author Manuel Penschuck
+ * @copyright to be decided
+ */
 #include <cmath>
 #include "PowerlawDegreeSequence.h"
-#include "defs.h"
 
-PowerlawDegreeSequence::PowerlawDegreeSequence(stxxl::int64 minDeg, stxxl::int64 maxDeg, double gamma, stxxl::int64 numNodes)
+PowerlawDegreeSequence::PowerlawDegreeSequence(int_t minDeg, int_t maxDeg, double gamma, int_t numNodes)
         : minDeg(minDeg), maxDeg(maxDeg), gamma(gamma), randomStream(numNodes), sortedRandomStream(randomStream, rand_less(), SORTER_MEM) {
 
     probabilitySum = 0;
--- include/PowerlawDegreeSequence.h
+++ include/PowerlawDegreeSequence.h
@@ -1,15 +1,18 @@
-//
-// Created by michael on 08.07.15.
-//
-
-#ifndef MYPROJECT_POWERLAWDEGREESEQUENCE_H
-#define MYPROJECT_POWERLAWDEGREESEQUENCE_H
-
+/**
+ * @file
+ * @brief Parameterizable Power-Law Distribution as STXXL stream
+ * @author Michael Hamann
+ * @author Manuel Penschuck
+ * @copyright to be decided
+ */
+#pragma once
 
 #include <stxxl/bits/common/types.h>
 #include "RandomStream.h"
 #include <functional>
 #include <stxxl/bits/stream/sort_stream.h>
+
+#include <defs.h>
 
 class PowerlawDegreeSequence {
 private:
@@ -18,27 +21,33 @@ private:
         double max_value() const { return 1.1; };
     };
 
-    stxxl::int64 minDeg;
-    stxxl::int64 maxDeg;
+    int_t minDeg;
+    int_t maxDeg;
     double gamma;
     double probabilitySum;
-    stxxl::int64 currentDegree;
+    int_t currentDegree;
     double nextThreshold;
 
     RandomStream randomStream;
-    typedef stxxl::stream::sort<RandomStream, rand_less> SortedRandomStream;
+    using SortedRandomStream = stxxl::stream::sort<RandomStream, rand_less>;
     SortedRandomStream sortedRandomStream;
 
     void findNextDegree();
 
 public:
-    typedef stxxl::int64 value_type;
-    PowerlawDegreeSequence(stxxl::int64 minDeg, stxxl::int64 maxDeg, double gamma, stxxl::int64 numNodes);
-    const stxxl::int64 & operator * () const { return currentDegree; };
-    const stxxl::int64 * operator -> () const { return &currentDegree; };
+    struct Parameters {
+        int_t minDegree;
+        int_t maxDegree;
+        int_t numberOfNodes;
+        double exponent;
+    };
+
+    using value_type = int_t ;
+
+    PowerlawDegreeSequence(int_t minDeg, int_t maxDeg, double gamma, int_t numNodes);
+    PowerlawDegreeSequence(const Parameters& p) : PowerlawDegreeSequence(p.minDegree, p.maxDegree, p.exponent, p.numberOfNodes) {}
+    const int_t & operator * () const { return currentDegree; };
+    const int_t * operator -> () const { return &currentDegree; };
     PowerlawDegreeSequence & operator++ () ;
     bool empty() const { return sortedRandomStream.empty(); };
 };
-
-
-#endif //MYPROJECT_POWERLAWDEGREESEQUENCE_H
