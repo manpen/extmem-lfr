6aecc5559b70f5e603387fe7aca1077628907abc
Michael Hamann <michael.hamann@kit.edu>
2016-02-05 17:20:40 +0100

    IMEdgeSwap: added wrapper for reading edge lists instead of IMGraph

--- include/EdgeSwaps/IMEdgeSwap.h
+++ include/EdgeSwaps/IMEdgeSwap.h
@@ -2,14 +2,24 @@
 
 #include <IMGraph.h>
 #include <EdgeSwaps/EdgeSwapBase.h>
+#include <IMGraphWrapper.h>
 #include <stxxl/vector>
 
 class IMEdgeSwap : public EdgeSwapBase {
 private:
+    IMGraphWrapper *_graph_wrapper;
     IMGraph &_graph;
     const swap_vector& _swaps;
 public:
-    IMEdgeSwap(IMGraph &graph, const stxxl::vector<SwapDescriptor>& swaps) : _graph(graph), _swaps(swaps) {};
+    IMEdgeSwap(IMGraph &graph, const stxxl::vector<SwapDescriptor>& swaps) : _graph_wrapper(0), _graph(graph), _swaps(swaps) {};
+
+    IMEdgeSwap(stxxl::vector<edge_t> &edges, const stxxl::vector<SwapDescriptor>& swaps) : _graph_wrapper(new IMGraphWrapper(edges)), _graph(_graph_wrapper->getGraph()), _swaps(swaps) {};
+
+    ~IMEdgeSwap() {
+        if (_graph_wrapper != 0) {
+            delete _graph_wrapper;
+        }
+    };
 
     void run() {
         swap_vector::bufreader_type swap_reader(_swaps);
@@ -27,6 +37,10 @@ public:
 #endif
 
             ++swap_reader;
+        }
+
+        if (_graph_wrapper != 0) {
+            _graph_wrapper->updateEdges();
         }
     };
 };
