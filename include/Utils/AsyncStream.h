18565e74d3a2557956942e1dabe95595edb11349
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2016-02-09 12:36:38 +0100

    Async stream can use as little as two buffers

--- include/Utils/AsyncStream.h
+++ include/Utils/AsyncStream.h
@@ -23,6 +23,7 @@ protected:
     StreamIn &  _producing_stream;
     uint32_t _producing_buffer_index;
     bool _producing_done;
+    bool _producing_buffer_avail;
     // consumer port
     uint32_t _consume_buffer_index;
 
@@ -42,20 +43,19 @@ protected:
         while(1) {
             // fill buffer
             auto & buffer = _buffers[_producing_buffer_index];
-            buffer.clear();
             buffer.reserve(_buffer_size);
             for(unsigned int i=0; i<_buffer_size && !_producing_stream.empty(); ++i, ++_producing_stream) {
                 buffer.push_back(*_producing_stream);
             }
 
             // advance to next index (indicate, that the recently filled buffer
-            // is available for consumption) but not before our new buffer is
-            // available
+            // is available for consumption)
             {
                 std::unique_lock<std::mutex> lock(_buffer_mutex);
                 auto next_idx = _next_buffer_index(_producing_buffer_index);
-                _buffer_cv.wait(lock, [&]() {return _consume_buffer_index != next_idx || _consume_done;});
-                _producing_buffer_index = next_idx;
+                _buffer_cv.wait(lock, [&]() {
+                    return _buffers[next_idx].empty() || _consume_done;
+                });
 
                 if (_consume_done)
                     break;
@@ -66,6 +66,7 @@ protected:
                     break;
                 }
 
+                _producing_buffer_index = next_idx;
                 _buffer_cv.notify_one();
             }
         }
@@ -79,6 +80,17 @@ protected:
         std::unique_lock<std::mutex> lock(_buffer_mutex);
 
         if (_consume_acquired) {
+            // underfull buffers are only allowed, if the input stream ended
+            if (_buffers[_consume_buffer_index].size() < _buffer_size) {
+                _consume_empty = true;
+                _consume_done = true;
+                return;
+            }
+
+            // indicated, that buffer may be used again
+            _buffers[_consume_buffer_index].clear();
+            _buffer_cv.notify_one();
+
             // since we are currently holding a buffer, let's give it back
             // by advancing the consuming index
             _consume_buffer_index = _next_buffer_index(_consume_buffer_index);
@@ -93,9 +105,7 @@ protected:
         _consume_read_iterator = _buffers[_consume_buffer_index].begin();
         _consume_end_iterator  = _buffers[_consume_buffer_index].end();
 
-        _consume_empty =
-            (_consume_buffer_index == _producing_buffer_index) ||
-            _buffers[_consume_buffer_index].empty();
+        _consume_empty = _buffers[_consume_buffer_index].empty();
 
         _buffer_cv.notify_one();
     }
@@ -120,6 +130,7 @@ public:
         , _producing_stream(stream)
         , _producing_buffer_index(0)
         , _producing_done(false)
+        , _producing_buffer_avail(false)
 
         , _consume_buffer_index(0)
         , _consume_acquired(false)
@@ -128,7 +139,7 @@ public:
 
         , _producing_thread(_producer_main, this)
     {
-        assert(number_of_buffers > 2);
+        assert(number_of_buffers >= 2);
         if (auto_acquire) acquire();
     }
 
--- test/TestAsyncStream.cpp
+++ test/TestAsyncStream.cpp
@@ -2,12 +2,13 @@
 #include <defs.h>
 #include <Utils/AsyncStream.h>
 
-class TestAsyncStreamStreamCentric : public ::testing::TestWithParam<std::tuple<bool,uint_t>> {};
+class TestAsyncStreamStreamCentric : public ::testing::TestWithParam<std::tuple<bool,uint_t, uint_t>> {};
 class TestAsyncStreamBufferCentric : public ::testing::TestWithParam<uint_t> {};
 
 TEST_P(TestAsyncStreamStreamCentric, orderedSequence) {
     bool   blockAccess    = std::get<0>(GetParam());
     uint_t sequenceLength = std::get<1>(GetParam());
+    uint_t numberOfBuffers= std::get<2>(GetParam());
 
     struct CounterStream {
         const uint_t _length;
@@ -21,7 +22,7 @@ TEST_P(TestAsyncStreamStreamCentric, orderedSequence) {
 
     CounterStream counterStream(sequenceLength);
     unsigned int block_size = 30000;
-    AsyncStream<CounterStream, uint_t> asyncStream(counterStream, true, block_size);
+    AsyncStream<CounterStream, uint_t> asyncStream(counterStream, true, block_size, numberOfBuffers);
 
     if (blockAccess) {
         unsigned int i = 0;
@@ -46,7 +47,7 @@ TEST_P(TestAsyncStreamStreamCentric, orderedSequence) {
 
 INSTANTIATE_TEST_CASE_P(TestAsyncStreamStreamCentricLengths,
     TestAsyncStreamStreamCentric,
-    ::testing::Combine(::testing::Bool(), ::testing::Values(0, 1, 2<<16, 2 << 20)));
+    ::testing::Combine(::testing::Bool(), ::testing::Values(0, 1, 2<<16, 2 << 20), ::testing::Values(2,3,4)));
 
 TEST_P(TestAsyncStreamBufferCentric, prematureConsumeStop) {
     uint_t buffers = GetParam();
