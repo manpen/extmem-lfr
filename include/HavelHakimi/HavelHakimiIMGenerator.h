d65dab1658cdf8c1288192b302e850f3c81b7681
Michael Hamann <michael.hamann@kit.edu>
2016-03-15 18:36:26 +0100

    HavelHakimiIMGenerator: add missing operator for stream

--- include/HavelHakimi/HavelHakimiIMGenerator.h
+++ include/HavelHakimi/HavelHakimiIMGenerator.h
@@ -244,6 +244,11 @@ public:
         return _current_edge;
     }
 
+    const value_type* operator->() const {
+        assert(_mode == Generate);
+        return &_current_edge;
+    }
+
     bool empty() const {
         assert(_mode == Generate);
         return _empty;
