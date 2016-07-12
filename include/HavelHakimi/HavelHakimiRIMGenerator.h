561f08b62236527a699549efce521340565a5fcd
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2016-07-12 17:23:59 +0200

    Added 'reverse' HavelHakimi. It currently iterates over a dlinked list; may get speed up by using a vector with lazy delete

--- include/HavelHakimi/HavelHakimiRIMGenerator.h
+++ include/HavelHakimi/HavelHakimiRIMGenerator.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <list>
 #include <assert.h>
 #include <stxxl/bits/common/utils.h>
 #include "defs.h"
@@ -11,6 +12,8 @@ class HavelHakimiRIMGenerator {
 public:
     //! Type of stream interface
     using value_type = edge_t;
+
+    static constexpr bool debug = false;
 
 protected:
     // Data keeping
@@ -25,155 +28,53 @@ protected:
         {}
 
         node_t size() const {
-            return node_upper - node_lower + 1;
+            return node_upper - node_lower;
         }
     };
 
     //! Stores block of nodes with same degree sorted by degree desc,
     //! i.e. _blocks.front().degree is smallest entry and has largest
     //! nodes. The ordering is established only after calling generate()
-    std::vector<Block> _blocks;
+    using BlockContainer = std::list<Block>;
+    BlockContainer _blocks;
 
     //! Push phase
     const node_t _initial_node;
     node_t _push_current_node;
-    edgeid_t _max_number_of_edges;
+    edgeid_t _degree_sum;
 
     //! Generation phase
+    edgeid_t _max_number_of_edges;
     PushTarget& _push_target;
 
-    /**
-     * Moves the block with highest degree from queue into stack and
-     * updates degree information. If remaining_neighbors indicates
-     * a future partial consumption, the block is automatically split
-     */
-    void _checkout_block() {
-        if (_blocks.empty()) {
-            std::cerr << "HH: Cannot materialize degree sequence. "
-                    "Node " << _current_edge.first << " requires "
-            << _remaining_neighbors << " more neighbors.\n";
-            _remaining_neighbors = 0;
-
-            _fetch_next_edge();
-
-            return;
-        }
-
-        auto block = _blocks.back();
-        if (block.size() > _remaining_neighbors) {
-            // partially consume block, i.e. keep lower ids at higher degree
-            _blocks.back().node_upper -= _remaining_neighbors;
-            block.node_lower = _blocks.back().node_upper+1;
-
-            // move unused block to stack
-            _blocks_checkedout.push(_blocks.back());
-            _blocks.pop_back();
-
-        } else {
-            // completely consume block, i.e. remove it from queue
-            // (may be reinserted with lower degree from stack)
-            _blocks.pop_back();
-        }
-
-        --block.degree;
-        _blocks_checkedout.push(block);
-
-        _current_edge.second = block.node_lower;
-        --_remaining_neighbors;
-    }
-
-    /**
-     * Returns blocks buffered in stack into queue and tries
-     * to merge neighboring blocks if degree and boundaries match
-     */
-    void _restore_blocks() {
-        // blocks with degree zero are kept in stack to avoid
-        // special treatment; they however can be destroyed
-        for (; !_blocks_checkedout.empty() && !_blocks_checkedout.top().degree; _blocks_checkedout.pop() );
-
-
-        // if stack is empty nothing is to be done
-        if (UNLIKELY(_blocks_checkedout.empty()))
-            return;
-
-        // make sure there is at least one block in the deque
-        // to avoid checking for corner cases in the for loop
-        if (UNLIKELY(_blocks.empty())) {
-            _blocks.push_back(_blocks_checkedout.top());
-            _blocks_checkedout.pop();
-        }
-
-        for(; !_blocks_checkedout.empty(); _blocks_checkedout.pop()) {
-            const auto & stack_block = _blocks_checkedout.top();
-            auto & deque_block = _blocks.back();
-
-            // test whether stack's block can be merged
-            if (UNLIKELY(
-                (stack_block.degree == deque_block.degree) &&
-                (stack_block.node_upper+1 == deque_block.node_lower)
-            )) {
-                deque_block.node_lower = stack_block.node_lower;
-            } else {
-                _blocks.push_back(stack_block);
-            }
+    void _print_blocks() {
+        if (debug) {
+           std::cerr << "[HH-RIM] blocks:";
+           for(const auto &b : _blocks)
+              std::cerr << " [L:" << b.node_lower << " U:" << b.node_upper << " D:" << b.degree << "] ";
+           std::cerr << std::endl;
         }
     }
 
-    void _fetch_next_edge() {
-        assert(!_empty);
 
-        if (LIKELY(_remaining_neighbors)) {
-            // advance to next neighbor
-            _current_edge.second++;
-
-            // if this new edge is not contained in the current block being consumed
-            // we have to fetch a new one
-            if (UNLIKELY(_blocks_checkedout.top().node_upper < _current_edge.second)) {
-                // will get correct id and also decrement _remaining_neighbors
-                _checkout_block();
-
-            } else {
-                --_remaining_neighbors;
-            }
-
-        } else {
-            // move blocks previously "parked" on stack back to queue
-            _restore_blocks();
-            _verify_deque_invariants();
-
-            if (UNLIKELY(_blocks.empty())) {
-                _empty = true;
-                return;
-            }
-
-            // take out first vertex from next block
-            auto & block = _blocks.back();
-            _current_edge.first = block.node_lower++;
-            _remaining_neighbors = block.degree;
-
-            // if this block contained only a single vertex, we can destroy it
-            if (block.node_lower > block.node_upper)
-                _blocks.pop_back();
-
-            _checkout_block();
-        }
-    }
 
     void _verify_block_invariants() {
 #ifndef NDEBUG
-        if (_blocks.empty())
-            return;
+        _print_blocks();
+
+        assert(!_blocks.empty());
 
         assert(_blocks.front().node_lower  >= _initial_node);
-        assert(_blocks.back().node_upper   <= _push_current_node-1);
+        assert(_blocks.back().node_upper   <= _push_current_node);
 
-        auto previous_block = _blocks.cbegin();
-        for (auto it = _blocks.cbegin()+1; it != _blocks.cend(); ++it) {
+        auto previous_block = _blocks.begin();
+
+        for (auto it = std::next(previous_block); it != _blocks.cend(); ++it) {
             auto & block = *it;
 
-            assert(block.degree < previous_block.degree);
+            assert(block.degree > previous_block->degree);
             assert(block.node_lower <= block.node_upper);
-            assert(block.node_lower == previous_block.node_upper+1);
+            assert(block.node_lower == previous_block->node_upper);
 
             previous_block++;
         }
@@ -182,93 +83,186 @@ protected:
 
 public:
     HavelHakimiRIMGenerator(PushTarget& push_target, node_t initial_node = 0) :
-        _mode(Push),
         _initial_node(initial_node),
         _push_current_node(initial_node),
+        _degree_sum(0),
         _max_number_of_edges(0),
         _push_target(push_target)
-    {}
+    {
+    }
 
     //! Push a new vertex -represented by its degree- into degree sequence
-    void push(degree_t deg) {
-        assert(_mode == Push);
+    void push(const degree_t & deg, const node_t nodes = 1) {
+        assert(nodes > 0);
         assert(deg > 0);
 
         if (UNLIKELY(_blocks.empty())) {
-           _blocks.push_back(Block(deg, _push_current_node, _push_current_node));
+           _blocks.push_back(Block(deg, _push_current_node, _push_current_node+nodes));
         } else {
-        if (LIKELY(_blocks.back().degree == deg)) {
-           _blocks.back().node_upper++;
-        } else {
-           assert(deg < _blocks.back().degree());
-           _blocks.push_back(Block(deg, _push_current_node, _push_current_node));
+           if (LIKELY(_blocks.back().degree == deg)) {
+              _blocks.back().node_upper += nodes;
+           } else {
+              assert(deg > _blocks.back().degree);
+              _blocks.push_back(Block(deg, _push_current_node, _push_current_node+nodes));
+           }
         }
 
-        ++_push_current_node;
-        _max_number_of_edges += deg;
+        _push_current_node += nodes;
+        _degree_sum += deg;
     }
 
     //! Switch to generation mode; the streaming interface become available
     void generate() {
-        assert(_mode == Push);
+        _max_number_of_edges = _degree_sum / 2;
 
-        _max_number_of_edges /= 2;
+        std::cout << "[HH-RIM] No. Blocks: " << _blocks.size() << ". No Nodes: " << (_push_current_node - _initial_node) << std::endl;
+        assert(_blocks.size());
 
-        std::cout << "HH Queue size: " << _blocks.size() << " for " << (_push_current_node - _initial_node) << " nodes\n";
+        edgeid_t edges_produced = 0;
+#ifndef NDEBUG
+        edge_t reader_edge = {0,0};
+#endif
+        auto emit_edges = [&] (const node_t& source, const node_t from, const degree_t no) {
+           for(node_t i = from; i < from + no; i++) {
+              _push_target.push({source, i});
+              if (debug)
+                 std::cerr << "[HH-RIM] push edge (" << source << ", " << i << ")" << std::endl;
+           }
 
-        // padding to allow safe writes in case of shifts w/o boundary checks
-        _verify_block_invariants();
+           if (debug) std::cerr << std::endl;
 
-        // There are the following update cases:
-        //  - no split: simply reduce degree by one
-        //  - split w/ deg-1 existing: split and merge
-        //  - split w/o deg-1 existing: insert new block and shift all remaining towards the end
-
-
-
-        auto begin = _blocks.begin();
-
-        auto decrement_degrees = [&] (_blocks::iterator it) {
-            while(it != _blocks.end()) {
-               it->degree--;
-               ++it;
-            }
+           edges_produced += no;
+#ifndef NDEBUG
+           edge_t this_edge{source, from + no - 1};
+           assert(this_edge.first < this_edge.second);
+           assert(this_edge.first >= _initial_node);
+           assert(this_edge.second < _push_current_node);
+           assert(reader_edge < this_edge);           
+           reader_edge = this_edge;
+#endif
         };
-
-        auto insert_and_decrement = [&] (_blocks::iterator it, const Block & new_block) {
-            auto tmp = new_block;
-            while(it != _blocks.end()) {
-               std::swap(tmp, *it);
-               tmp.degree--;
-               ++it;
-            }
-            _blocks.push_back(tmp);
-        };
-
-
+#ifndef NDEBUG
+        auto last_total_degree = std::accumulate(_blocks.begin(), _blocks.end(), edgeid_t{0},
+             [] (const edgeid_t& i, const Block& blk) {return i+blk.size()*blk.degree;});
+#endif
         while(!_blocks.empty()) {
             _verify_block_invariants();
-
+            
             // extract current_node and remove first block if now empty
-            auto current_node = begin.node_lower++;
-            auto degree_remain = begin.degree;
-            if (UNLIKELY(begin_it.node_lower >= begin_it.node_upper))
-               ++begin;
-
-            while(degree_remain) {
-               if (UNLIKELY(last->size() > degree_remain)) {
-
-
-
+            auto current_node = _blocks.front().node_lower++;
+            const auto degree = _blocks.front().degree;
+            
+            if (UNLIKELY(!_blocks.front().size())) {
+               _blocks.pop_front();
             }
-            
-        }
-            
 
+            assert(degree);
+
+            degree_t degree_remain = degree;
+            edges_produced = 0;
+            auto reader = std::prev(_blocks.end());
+            while(degree_remain) {
+               if (UNLIKELY(!reader->degree)) {
+                  break;
+               }
+
+               if (LIKELY(reader->size() <= degree_remain)) {
+                  if (debug)
+                     std::cout << "Red Deg" << std::endl;
+                  
+                  // we just decrease the block's degree and (later) connect to
+                  // vertices contained
+                  reader->degree--;
+                  degree_remain -= reader->size();
+                  reader--;
+
+               } else {
+                  // we have to split the block
+                  if (reader != _blocks.begin() && std::prev(reader)->degree+1 == reader->degree) {
+                     if (debug)
+                        std::cout << "Split Merge Prev " << std::endl;
+
+                     // but we can merge the split block with the previous one
+                     auto prev = std::prev(reader);
+                     emit_edges(current_node, prev->node_upper, degree_remain);
+                     prev->node_upper += degree_remain;
+                     reader->node_lower = prev->node_upper;
+
+                  } else if(std::next(reader) != _blocks.end() && (reader->degree == std::next(reader)->degree)) {
+                     if (debug)
+                        std::cout << "Split Merge Next" << std::endl;
+                     // but we can merge the remaining part of the split with the next block
+                     auto next = std::next(reader);
+
+                     emit_edges(current_node, reader->node_lower, degree_remain);
+                     emit_edges(current_node, next->node_lower, next->size());
+
+                     reader->degree--;
+                     reader->node_upper = reader->node_lower + degree_remain;
+                     next->node_lower = reader->node_upper;
+
+                     reader = next;
+
+                  } else {
+                     if (debug)
+                        std::cout << "Split" << std::endl;
+                     // but we cannot merge and have to shift all blocks
+                     emit_edges(current_node, reader->node_lower, degree_remain);
+
+                     reader = std::next(_blocks.insert(reader, {reader->degree-1, reader->node_lower, reader->node_lower + degree_remain}));
+                     // reduce size of unused block
+                     reader->node_lower += degree_remain;
+                  }  
+                  degree_remain = 0;
+               }
+
+               _print_blocks();
+            }
+
+            {
+               auto prev = reader;
+               reader++;
+               if (reader != _blocks.end()) {
+                  emit_edges(current_node, reader->node_lower, _blocks.back().node_upper - reader->node_lower);
+
+                  // merge with previous ?
+                  if (UNLIKELY(_blocks.size() > 1 &&  prev->degree == reader->degree)) {
+                     if (debug) {
+                        std::cout << "Merge" << std::endl;
+                        _print_blocks();
+                     }
+
+                     prev->node_upper = reader->node_upper;
+                     _blocks.erase(reader);
+                  }   
+               }
+            }
+
+            assert(edges_produced == degree - degree_remain);
+
+            if (UNLIKELY(degree_remain)) {
+               std::cerr << "[HH-RIM] Node " << current_node << " has unsatisfied degree of " << degree_remain << ".\n"; 
+            }
+
+            while(UNLIKELY(!_blocks.empty() && !_blocks.front().degree))
+               _blocks.pop_front();
+
+#ifndef NDEBUG
+            auto total_degree = std::accumulate(_blocks.begin(), _blocks.end(), edgeid_t{0},
+                  [] (const edgeid_t& i, const Block& blk) {return i+blk.size()*blk.degree;});
+            assert(last_total_degree - total_degree == 2*edges_produced);
+            last_total_degree = total_degree;
+#endif
+        }
+
+        if (debug)
+           std::cerr << "[HH-RIM] Blocks remaining in buffer: " << _blocks.size() << std::endl;
+
+        _blocks.clear();
     }
 
+    // Value is valid after \generate was called
     edgeid_t maxEdges() const {
-        assert(_mode == Generate);
         return _max_number_of_edges;
     }
 };
---
+++ test/TestHavelHakimiRIMGenerator.cpp
@@ -0,0 +1,183 @@
+#include <gtest/gtest.h>
+#include <HavelHakimi/HavelHakimiRIMGenerator.h>
+
+#include <stxxl/stream>
+#include <stxxl/bits/common/rand.h>
+
+class TestHavelHakimiIMGenerator : public ::testing::Test {
+protected:
+    template <typename ValueType>
+    struct PushToVector {
+       std::vector<ValueType> vector;
+       void push(const ValueType& v) {
+          vector.push_back(v);
+       }
+    };
+
+    void _check_hh(const std::vector<degree_t> & sequence, node_t initial_id, bool debug = false, bool strict = false) {
+        edgeid_t total_degree = 0;
+        PushToVector<edge_t> edges;
+        HavelHakimiRIMGenerator<PushToVector<edge_t>> hh(edges, initial_id);
+
+        for (auto &d : sequence) {
+            total_degree += d;
+            hh.push(d);
+        }
+
+        // stream must be only available after completely pushed
+        hh.generate();
+
+        // count degrees
+        std::vector<degree_t> degrees(sequence.size(), 0);
+        edge_t previous_edge = {-1, -1};
+        for(const auto & edge : edges.vector) {
+            if (debug)
+                std::cout << "Got edge " << edge << std::endl;
+
+            // no self-loops and ordered entries
+            ASSERT_LT(edge.first, edge.second);
+
+            // globally ordered
+            ASSERT_LT(previous_edge, edge);
+
+            ASSERT_GE(edge.first, initial_id);
+            ASSERT_LE(edge.first, static_cast<node_t>(initial_id+sequence.size()-1));
+            ASSERT_GE(edge.second, initial_id);
+            ASSERT_LE(edge.second, static_cast<node_t>(initial_id+sequence.size()-1));
+
+            degrees.at(edge.first - initial_id)++;
+            degrees.at(edge.second - initial_id)++;
+
+            previous_edge = edge;
+        }
+
+        // compare
+        if (!strict) {
+            // if errors are allowed, the degrees might not be ordered
+            //std::sort(degrees.begin(), degrees.end());
+        }
+
+        for(unsigned int i=0; i<sequence.size(); i++) {
+            degree_t required = sequence[i];
+
+            if (debug)
+                std::cout << "Node " << i << " requested " << sequence[i] << " got " << required << std::endl;
+
+            if (strict)
+                ASSERT_EQ(degrees[i], required) << "node: " << i;
+
+            ASSERT_LE(degrees[i], required) << "node: " << i;
+        }
+
+        ASSERT_GT(static_cast<edgeid_t>(edges.vector.size()), total_degree / 3);
+        ASSERT_LE(static_cast<edgeid_t>(edges.vector.size()), total_degree / 2);
+    }
+
+    std::vector<degree_t> _barabasi(node_t nodes, degree_t edges_per_node) {
+        edgeid_t no_edges = 3 + nodes*edges_per_node;
+
+        std::vector<node_t> graph;
+        graph.reserve(2*no_edges);
+
+        stxxl::random_number32 rand;
+
+        // generate barabasi albert pref attachment graph with an
+        // initial (edges_per_node)-start
+        for(edgeid_t i=0; i<edges_per_node; i++) {
+            graph.push_back(i+1);
+            graph.push_back(0);
+        }
+
+        // preferential attachment phase
+        std::set<node_t> previous_neighbors;
+        for(node_t u=0; u < nodes; u++) {
+            previous_neighbors.clear();
+            for(degree_t e=0; e < edges_per_node; e++) {
+                node_t v = u + edges_per_node + 1;
+                graph.push_back(v);
+
+                // select neighbor and ensure graph remains simple
+                node_t n = v;
+                while(n == v || previous_neighbors.count(n)) {
+                    n = graph.at(rand(graph.size()));
+                }
+                previous_neighbors.insert(n);
+
+                graph.push_back(n);
+            }
+        }
+
+        // compute degrees
+        std::vector<degree_t> degrees(nodes + 4);
+        for(auto & u : graph) {
+            degrees.at(u)++;
+        }
+
+        return degrees;
+    }
+};
+
+
+TEST_F(TestHavelHakimiIMGenerator, clique) {
+    std::vector<unsigned int> sizes({3,4,5,6,1000,1001});
+    for(auto nodes : sizes) {
+        std::vector<degree_t> seq(nodes, static_cast<degree_t>(nodes - 1));
+        this->_check_hh(seq, 0, false, true);
+        this->_check_hh(seq, 1234, false, true);
+
+    }
+}
+
+
+TEST_F(TestHavelHakimiIMGenerator, circle) {
+    std::vector<unsigned int> sizes({3,4,5,6,1000,1001});
+    for(auto nodes : sizes) {
+        std::vector<degree_t> seq(nodes, static_cast<degree_t>(2));
+        this->_check_hh(seq, 0, false, true);
+        this->_check_hh(seq, 1234, false, true);
+    }
+}
+
+TEST_F(TestHavelHakimiIMGenerator, worstCase) {
+    std::vector<node_t> sizes({2,4,6,8,100,1000});
+    for(auto nodes : sizes) {
+        std::vector<degree_t> seq(nodes);
+
+        for(auto i=0; i < nodes; i++)
+           seq[i] = 1 + i/2;
+
+
+        this->_check_hh(seq, 0, false, true);
+        this->_check_hh(seq, 1234, false, true);
+    }
+}
+
+TEST_F(TestHavelHakimiIMGenerator, barabasiAlbert) {
+    unsigned int iterations = 100;
+    unsigned int nodes = 1000;
+    unsigned int edges_per_node = 3;
+
+    for(unsigned int iter=0; iter < iterations; iter++) {
+        auto degrees = this->_barabasi(nodes, edges_per_node);
+
+        std::sort(degrees.begin(), degrees.end());
+        this->_check_hh(degrees, 0, false, true);
+    }
+}
+
+TEST_F(TestHavelHakimiIMGenerator, barabasiAlbertDefect) {
+    unsigned int iterations = 100;
+    unsigned int nodes = 1000;
+    unsigned int edges_per_node = 3;
+
+    for(unsigned int iter=0; iter < iterations; iter++) {
+        auto degrees = this->_barabasi(nodes, edges_per_node);
+        stxxl::random_number32 rand;
+
+        for(unsigned int d=rand(10); d; d--)
+            degrees[rand(degrees.size())] += rand(nodes/2);
+
+        std::sort(degrees.begin(), degrees.end());
+        this->_check_hh(degrees, 0, false);
+    }
+}
