7346a1bb69007ed0ff669c217f767bedc3a96ff2
Michael Hamann <michael.hamann@kit.edu>
2016-07-13 18:22:21 +0200

    Add a test case for community rewiring

---
+++ test/TestCommunityRewiring.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include <LFR/CommunityEdgeRewiringSwaps.h>
+
+class TestCommunityRewiring : public ::testing::Test { };
+
+TEST_F(TestCommunityRewiring, testTwoCommunities) {
+	using edge_community_vector_t = stxxl::vector<LFR::CommunityEdge>;
+
+	edge_community_vector_t edges;
+	edges.push_back(LFR::CommunityEdge(0, edge_t(0, 1)));
+	edges.push_back(LFR::CommunityEdge(1, edge_t(0, 1)));
+	edges.push_back(LFR::CommunityEdge(0, edge_t(2, 3)));
+	edges.push_back(LFR::CommunityEdge(1, edge_t(2, 3)));
+	edges.push_back(LFR::CommunityEdge(1, edge_t(3, 4)));
+	edges.push_back(LFR::CommunityEdge(0, edge_t(5, 6)));
+
+	CommunityEdgeRewiringSwaps rewiring(edges, 200);
+	rewiring.run();
+
+	LFR::CommunityEdge prev = LFR::CommunityEdge(0, edge_t::invalid());
+	for (auto it = edges.cbegin(); it != edges.cend(); ++it) {
+		EXPECT_NE(prev.edge, it->edge);
+	}
+};
