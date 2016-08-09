5a6aa452febcdb6ab8883a8d5d59bdd181a219fe
Michael Hamann <michael@content-space.de>
2016-08-09 17:56:35 +0200

    Test SemiLoadedEdgeSwapTFP with more cases

--- test/TestSemiLoadedSwaps.cpp
+++ test/TestSemiLoadedSwaps.cpp
@@ -6,6 +6,7 @@ class TestSemiLoadedSwaps : public ::testing::Test {};
 #ifdef EDGE_SWAP_DEBUG_VECTOR
 
 #include <EdgeSwaps/EdgeSwapInternalSwaps.h>
+#include <EdgeSwaps/SemiLoadedEdgeSwapTFP.h>
 #include <EdgeStream.h>
 #include <defs.h>
 
@@ -17,7 +18,7 @@ TEST_F(TestSemiLoadedSwaps, testOnlySemiLoadedSwaps) {
     edge_list.push({6, 7});
     edge_list.consume();
 
-    EdgeSwapInternalSwaps algo(edge_list);
+    EdgeSwapTFP::SemiLoadedEdgeSwapTFP algo(edge_list);
 
     algo.push(SemiLoadedSwapDescriptor {edge_t {0, 1}, 1, true});
     algo.push(SemiLoadedSwapDescriptor {edge_t {4, 5}, 3, true});
@@ -54,7 +55,7 @@ TEST_F(TestSemiLoadedSwaps, testCombinedSwaps) {
     edge_list.push({6, 7});
     edge_list.consume();
 
-    EdgeSwapInternalSwaps algo(edge_list);
+    EdgeSwapTFP::SemiLoadedEdgeSwapTFP algo(edge_list);
 
     algo.push(SwapDescriptor {0, 1, true});
     algo.push(SemiLoadedSwapDescriptor {edge_t {4, 5}, 3, true});
@@ -78,6 +79,54 @@ TEST_F(TestSemiLoadedSwaps, testCombinedSwaps) {
     ASSERT_EQ(*edge_list, edge_t(2, 5));
     ++edge_list;
     ASSERT_EQ(*edge_list, edge_t(3, 4));
+}
+
+TEST_F(TestSemiLoadedSwaps, testLoadedIsSameAsId) {
+    EdgeStream edge_list;
+    edge_list.push({0, 1});
+    edge_list.push({2, 3});
+    edge_list.push({4, 5});
+    edge_list.push({6, 7});
+    edge_list.consume();
+
+    EdgeSwapTFP::SemiLoadedEdgeSwapTFP algo(edge_list);
+
+    std::vector<edge_t> update_edges;
+    algo.setUpdatedEdgesCallback([&](EdgeSwapTFP::SemiLoadedEdgeSwapTFP::edge_update_sorter_t &it) {
+            while (!it.empty()) {
+                update_edges.emplace_back(it->first, it->second);
+                ++it;
+            }
+        });
+
+    algo.push(SemiLoadedSwapDescriptor {edge_t {0, 1}, 0, true});
+    algo.push(SemiLoadedSwapDescriptor {edge_t {2, 3}, 0, true});
+    algo.push(SemiLoadedSwapDescriptor {edge_t {2, 3}, 3, true});
+    algo.push(SemiLoadedSwapDescriptor {edge_t {4, 5}, 2, true});
+
+    algo.run();
+
+    auto & debug = algo.debugVector();
+
+    ASSERT_EQ(debug.size(), 2u);
+
+    ASSERT_TRUE(debug[0].performed);
+    ASSERT_TRUE(debug[1].performed);
+
+    edge_list.rewind();
+    ASSERT_EQ(*edge_list, edge_t(0, 7));
+    ++edge_list;
+    ASSERT_EQ(*edge_list, edge_t(1, 2));
+    ++edge_list;
+    ASSERT_EQ(*edge_list, edge_t(3, 6));
+    ++edge_list;
+    ASSERT_EQ(*edge_list, edge_t(4, 5));
+
+    ASSERT_EQ(update_edges.size(), 4u);
+    ASSERT_EQ(update_edges[0], edge_t(0, 7));
+    ASSERT_EQ(update_edges[1], edge_t(1, 2));
+    ASSERT_EQ(update_edges[2], edge_t(3, 6));
+    ASSERT_EQ(update_edges[3], edge_t(4, 5));
 }
 #else
 TEST_F(TestSemiLoadedSwaps, warning) {
