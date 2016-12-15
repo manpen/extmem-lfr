cb15028585338ae5e4ae3db399d6fad511e6ad9a
hung <thehung@hotmail.de>
2016-12-15 20:45:10 +0100

    Added exec

---
+++ main_cm_emtfp_convergence.cpp
@@ -0,0 +1,16 @@
+//
+// Created by hungt on 15.12.16.
+//
+
+#include <defs.h>
+
+int main(int argc, char* argv[]) {
+
+    const node_t num_nodes = 1000000;
+    const degree_t min_deg = 10;
+    const degree_t max_deg = num_nodes / 10;
+
+
+    return 0;
+
+}
